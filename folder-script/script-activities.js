2ff5d385dc7af1dc3d9074d76a7577a98d382b84
ColKol <coliemail.07@gmail.com>
2023-10-30 16:31:00 +0800

    updated images

--- folder-script/script-activities.js
+++ folder-script/script-activities.js
@@ -7,23 +7,20 @@ var global_category;
 
 // open JSON file
 fetch('folder-json/data-activities.json')
-    .then(response => response.json())
-    .then(data => {
-    createProjects(data);
-})
-  .catch(error => {
-    console.log('Error:', error);
-});
+  .then(response => response.json())
+  .then(data => {createProjects(data)})
+  .catch(error => {console.log('Error:', error);});
 
+// setup each project's buttons
 function createProjects(all_projects){
   let projects = document.getElementById("project-box-holder");
   projects.innerHTML = "";
 
-  for (let i = 0; i < Object.keys(all_projects).length; i++){
-    // loop through activities and and add their popup buttons 
-    let projectName = all_projects[i]["name"];
-    let projectThumbnail = all_projects[i]["thumbnail"]
-    projects.innerHTML += `<button onclick="openPopup(${projectName})" class="project-button"> <div class="project-box" style="background-image: linear-gradient(var(--icon-gradient), var(--icon-gradient)), url('img-all/img-activities/img-${projectName.toLowerCase()}/${projectThumbnail}')"> <h1> ${projectName} </h1> </div> </button>`;
+  // loop through activities and and add their popup buttons 
+  for (let i = Object.keys(all_projects).length - 1; i >= 0; i--){
+  let projectName = all_projects[i]["name"];
+  let projectThumbnail = all_projects[i]["thumbnail"]
+  projects.innerHTML += `<button onclick="openPopup(${projectName})" class="project-button"> <div class="project-box" style="background-image: linear-gradient(var(--icon-gradient), var(--icon-gradient)), url('img-all/img-activities/img-${projectName.toLowerCase()}/${projectThumbnail}')"> <h1> ${projectName} </h1> </div> </button>`;
   }
 }
 
